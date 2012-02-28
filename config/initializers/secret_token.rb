becc9246a84a9bd19bfc06cc71100293c0a942a5
Siyi Gu <nodnightofday@gmail.com>
2012-02-28 14:12:25 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WcsMentoringProject::Application.config.secret_token = '9cbc1c39eb042228a17cb24df1cc2e25e74b7bb8722aefd054c39f67a8e0a19ef0effbc339eb6a39a9af8337758523a0ed56274884c4fb6e495f239282488fef'
